e94765d251a99528158daf545a2eb3b108c5a9dd
j0verton <overtonje@gmail.com>
2020-10-09 13:33:50 -0500

    working on article list

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -1,8 +1,24 @@
 import React from "react"
 import "./Animal.css"
 import { Link } from "react-router-dom"
+import { Container } from 'semantic-ui-react'
 
 export const Article = ({ article }) => (
-    
+    <Container className="article--container" >
+        <Header as='h3'>{article.title}</Header>
+        <p>Posted by: {article.user.name}</p>
+        <p>Date: {new Date(article.date).toLocaleDateString('en-US')}</p>
+        <p>{article.synopsis}</p>
+                    <div class="article--actions">
+                <a href={article.url} target="_blank">
+                    Read More
+                </a>
+
+                <div class="formBtns">
+                    <button id="editArticle--${article.id}" class="editBtn">Edit</button>
+                    <button id="deleteArticle--${article.id}" class="trashBtn">🗑️</button>
+                </div>
+            </div>
+    </Container>
 
 )
---
+++ src/components/articles/ArticleList.js
@@ -0,0 +1,5 @@
+import React, { useContext, useEffect, useState } from "react"
+import { ArticleContext } from "./ArticleProvider"
+import { ArticleCard } from "./ArticleCard"
+import "./Article.css"
+import { useHistory } from "react-router-dom"
