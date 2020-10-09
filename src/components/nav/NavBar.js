ed8b5a2c92bbde62741884990c9a9b22e41787c7
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-09 11:25:26 -0500

    updated NavBar for visualization

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar--item active">
+                <Link className="navbar--link" to="/">Home</Link>
+            </li>
+
+            <li className="navbar--item">
+                <Link className="navbar--link" to="/">Test 2</Link>
+            </li>
+
+            <li className="navbar--item">
+                <Link className="navbar--link" to="/">Test 3</Link>
+            </li>
+
+            <li className="navbar--item">
+                <Link className="navbar--link" to="/">Test 4</Link>
+            </li>
+        </ul>
+    )
+}
