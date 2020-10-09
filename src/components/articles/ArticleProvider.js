27eabde4c0524dab82b282e719a732f8a7aeec50
j0verton <overtonje@gmail.com>
2020-10-09 11:39:51 -0500

    built article provider

---
+++ src/components/articles/ArticleProvider.js
@@ -0,0 +1,54 @@
+import React, { useState, createContext } from "react"
+
+export const ArticleContext = createContext()
+
+export const ArticleProvider = (props) => {
+    const [articles, setArticles] = useState([])
+
+    const getArticles = () => {
+        return fetch('http://localhost:8088/news?_expand=user')
+        .then(response => response.json())
+    }
+    
+    // adds new articles to database
+    const saveArticle = artObj => {
+        return fetch('http://localhost:8088/news', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(artObj)
+        })
+    }
+    
+    // allows user to edit their articles
+    export const editArticle = artObj => {
+        fetch(`http://localhost:8088/news/${artObj.id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(artObj)
+        })
+    }
+    
+    // removes article from database
+    export const deleteArticle = articleId => {
+        return fetch(`http://localhost:8088/news/${articleId}`, {
+            method: 'DELETE'
+        })
+    }
+
+    export const getArticleById = (id) => {
+        return fetch('http://localhost:8088/news/${articleId}?_expand=users')
+            .then(res => res.json())
+    }
+
+    return (
+        <ArticleContext.Provider value={{
+            articles, getArticleById, getArticles, saveArticle, editArticle, deleteArticle
+        }}>
+            {props.children}
+        </ArticleContext.Provider>
+    )
+}
