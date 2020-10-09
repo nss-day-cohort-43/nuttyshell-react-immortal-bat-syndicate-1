9cb0bc3a77db928e7eef3051e2c463e09564ca1b
Ricky Blake <rab0196@yahoo.com>
2020-10-09 11:52:46 -0500

    Changed registration header

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -56,7 +56,7 @@ export const Register = (props) => {
             </dialog>
 
             <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Please Register for NSS Kennels</h1>
+                <h1 className="h3 mb-3 font-weight-normal">Please Register for IBS</h1>
                 <fieldset>
                     <label htmlFor="firstName"> First Name </label>
                     <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
