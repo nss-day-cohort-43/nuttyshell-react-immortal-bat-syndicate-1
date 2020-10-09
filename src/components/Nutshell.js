c2effafd10f5788b025cf7ad5184d44334ab1dec
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-09 11:33:29 -0500

    successfully rendering skeleton to dom

--- src/components/Nutshell.js
+++ src/components/Nutshell.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { Route } from "react-router-dom"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
 import "./Nutshell.css"
 
 export const Nutshell = () => (
     <>
-        <h2>Nutshell</h2>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from "react-router-dom"
+import { Nutshell } from "./components/Nutshell"
 import './index.css'
-import Nutshell from './components/Nutshell'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Nutshell />
+    <Router>
+      <Nutshell />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
