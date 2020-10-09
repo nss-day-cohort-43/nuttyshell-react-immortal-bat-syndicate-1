33f4d0b3ddb83f0d82066706e72a4f2ce13c3308
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-09 16:28:03 -0500

    form interacts with database, working on textarea bug

--- src/components/chat/ChatForm.js
+++ src/components/chat/ChatForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useEffect } from "react"
+import React, { useContext, useRef, useEffect, useState } from "react"
 import { ChatContext } from "./ChatProvider"
 import { useHistory } from "react-router-dom"
 import { Button, Form, MessageContent, TextArea } from "semantic-ui-react"
@@ -8,15 +8,17 @@ import "./Chat.css"
 export const ChatForm = (props) => {
     const { addMessage } = useContext(ChatContext)
 
-    const message = useRef(null)
+    const content = useRef(null)
 
     useEffect(() => {
     }, [])
 
     const constructNewMessage = () => {
         addMessage({
-            message: MessageContent.current.value
+            message: content.current.value,
+            date: new Date().toLocaleString("en-US")
         })
+            .then(() => history.push("/messages"))
     }
 
     const handleSubmit = evt => {
@@ -24,13 +26,16 @@ export const ChatForm = (props) => {
         constructNewMessage()
     }
 
+    const history = useHistory()
+
     return (
         <Form className="messageForm" onSubmit={handleSubmit}>
             <h2 className="messageForm--title">New Message</h2>
 
             <fieldset>
                 <div className="form-group">
-                    <TextArea id="messageContent" name="messageContent" autoFocus required className="form-control" placeholder="Enter your message here..." style={{ minHeight: 100 }} />
+                    <TextArea id="messageContent" ref={content} className="form-control"
+                        placeholder="Enter your message here..." autoFocus required style={{ minHeight: 100 }} />
                 </div>
             </fieldset>
 
