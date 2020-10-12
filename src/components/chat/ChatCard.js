19b6e324dccbadd1bfd2ea1956135a9220766843
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-11 21:38:33 -0500

    working on edit/delete functionality

--- src/components/chat/ChatCard.js
+++ src/components/chat/ChatCard.js
@@ -6,7 +6,7 @@ import "./Chat.css"
 export const ChatCard = ({ message }) => (
     <section className="message">
         <p className="message--content">{message.message}</p>
-        <p classNAme="message--poster">{message.user.username}</p>
+        <p className="message--poster">{message.user.username}</p>
         <p className="message--date">{message.date}</p>
     </section>
 )
