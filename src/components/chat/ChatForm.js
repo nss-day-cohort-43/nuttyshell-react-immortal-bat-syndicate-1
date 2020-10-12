e410688f215a9c048537b6932fe60f6b34cb53f5
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-11 21:28:52 -0500

    adding messages works

--- src/components/chat/ChatForm.js
+++ src/components/chat/ChatForm.js
@@ -1,21 +1,22 @@
-import React, { useContext, useRef, useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom"
 import { ChatContext } from "./ChatProvider"
-import { useHistory } from "react-router-dom"
-import { Button, Form, MessageContent, TextArea } from "semantic-ui-react"
+import { Button, Form } from "semantic-ui-react"
 import 'semantic-ui-css/semantic.min.css'
 import "./Chat.css"
 
 export const ChatForm = (props) => {
     const { addMessage } = useContext(ChatContext)
 
-    const content = useRef(null)
+    const [message, setMessage] = useState({})
 
     useEffect(() => {
     }, [])
 
     const constructNewMessage = () => {
         addMessage({
-            message: content.current.value,
+            sendingUserId: parseInt(localStorage.getItem("nutty_customer")),
+            message: message.content,
             date: new Date().toLocaleString("en-US")
         })
             .then(() => history.push("/messages"))
@@ -26,17 +27,31 @@ export const ChatForm = (props) => {
         constructNewMessage()
     }
 
+    const handleInputChange = (evt) => {
+        const newMessage = { ...message }
+        newMessage[evt.target.name] = evt.target.value
+        setMessage(newMessage)
+    }
+
     const history = useHistory()
 
     return (
-        <Form className="messageForm" onSubmit={handleSubmit}>
-            <h2 className="messageForm--title">New Message</h2>
+        <>
+            <Form className="messageForm" onSubmit={handleSubmit}>
+                <h2 className="messageForm--title">New Message</h2>
 
-            <Form.TextArea placeholder='Send a message..' />
+                <Form.Input
+                    required
+                    placeholder='Send a message..'
+                    id='messageContent'
+                    name='content'
+                    onChange={handleInputChange}
+                />
 
-            <Button type="submit" primary compact required className="btn btn-primary">
-                Send
+                <Button type="submit" primary compact required className="btn btn-primary">
+                    Send
             </Button>
-        </Form >
+            </Form >
+        </>
     )
 }
