499bdd5749eca814470e62927fe80dd56411988e
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-12 09:37:36 -0500

    delete functionality works

--- src/components/chat/ChatCard.js
+++ src/components/chat/ChatCard.js
@@ -1,16 +1,32 @@
-import React from "react"
+import React, { useContext } from "react"
+import { useHistory } from "react-router-dom"
 import { ChatContext } from "./ChatProvider"
-import { Button, Icon } from "semantic-ui-react"
+import { Button, Container, Icon } from "semantic-ui-react"
 import 'semantic-ui-css/semantic.min.css'
 import "./Chat.css"
 
-export const ChatCard = ({ message }) => (
-    <section className="message">
-        <p className="message--content">{message.message}</p>
-        <p className="message--poster">{message.user.username}</p>
-        <p className="message--date">{message.date}</p>
-        <Button icon compact id={message.id}>
-            <Icon name="delete" />
-        </Button>
-    </section>
-)
+export const ChatCard = ({ message }) => {
+    const { deleteMessage } = useContext(ChatContext)
+    const history = useHistory()
+
+    return (
+        <Container className="message--container">
+            <section className="message">
+                <p className="message--content">{message.message}</p>
+                <p className="message--poster">{message.user.username}</p>
+                <p className="message--date">{message.date}</p>
+
+                <div className="messageBtns">
+                    {message.userId === parseInt(localStorage.getItem("nutty_customer")) ?
+                        <>
+                            <Button icon compact id="deleteMessage--" className="deleteBtn" onClick={
+                                () => deleteMessage(message.id)
+                            }><Icon name="delete" />
+                            </Button>
+                        </>
+                        : null}
+                </div>
+            </section>
+        </Container>
+    )
+}
