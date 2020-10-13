e6e1b06b7d59535603399fc8fffd7161c9b4df83
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-13 17:51:47 -0500

    added video bg to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react"
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom"
+import video from "../../video/ibs-video.mp4"
 import "./Login.css"
 
 
@@ -30,34 +31,40 @@ export const Login = props => {
     }
 
     return (
-        <main className="container--login">
-            <dialog className="dialog dialog--auth" ref={existDialog}>
-                <div>User does not exist</div>
-                <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
-            </dialog>
+        <>
+            <main className="container--login">
+                <dialog className="dialog dialog--auth" ref={existDialog}>
+                    <div>User does not exist</div>
+                    <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+                </dialog>
 
-            <section>
-                <form className="form--login" onSubmit={handleLogin}>
-                    <h1>IBS</h1>
-                    <h2>Please sign in</h2>
-                    <fieldset>
-                        <label htmlFor="inputEmail"> Email address </label>
-                        <input ref={email} type="email"
-                            id="email"
-                            className="form-control"
-                            placeholder="Email address"
-                            required autoFocus />
-                    </fieldset>
-                    <fieldset>
-                        <button type="submit">
-                            Sign in
+                <section>
+                    <form className="form--login" onSubmit={handleLogin}>
+                        <h1>IBS</h1>
+                        <h2>Please sign in</h2>
+                        <fieldset>
+                            <label htmlFor="inputEmail"> Email address </label>
+                            <input ref={email} type="email"
+                                id="email"
+                                className="form-control"
+                                placeholder="Email address"
+                                required autoFocus />
+                        </fieldset>
+                        <fieldset>
+                            <button type="submit">
+                                Sign in
                         </button>
-                    </fieldset>
-                </form>
-            </section>
-            <section className="link--register">
-                <Link to="/register">Not a member yet?</Link>
-            </section>
-        </main>
+                        </fieldset>
+                    </form>
+                </section>
+                <section className="link--register">
+                    <Link to="/register">Not a member yet?</Link>
+                </section>
+            </main>
+
+            <video className="videoTag" autoPlay loop muted>
+                <source src={video} type="video/mp4" />
+            </video>
+        </>
     )
 }
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react"
 import { useHistory } from "react-router-dom"
+import video from "../../video/ibs-video.mp4"
 import "./Login.css"
 
 export const Register = (props) => {
@@ -53,39 +54,45 @@ export const Register = (props) => {
                     conflictDialog.current.showModal()
                 }
             })
-        
+
     }
 
     return (
-        <main style={{ textAlign: "center" }}>
+        <>
+            <main style={{ textAlign: "center" }}>
 
-            <dialog className="dialog dialog--password" ref={conflictDialog}>
-                <div>Account with that email address or username already exists</div>
-                <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
-            </dialog>
+                <dialog className="dialog dialog--password" ref={conflictDialog}>
+                    <div>Account with that email address or username already exists</div>
+                    <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
+                </dialog>
 
-            <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Please Register for IBS</h1>
-                <fieldset>
-                    <label htmlFor="firstName"> First Name </label>
-                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="lastName"> Last Name </label>
-                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="username"> Username </label>
-                    <input ref={username} type="text" name="username" className="form-control" placeholder="Username" required />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="inputEmail"> Email address </label>
-                    <input ref={email} type="email" name="email" className="form-control" placeholder="Email address" required />
-                </fieldset>
-                <fieldset>
-                    <button type="submit"> Sign in </button>
-                </fieldset>
-            </form>
-        </main>
+                <form className="form--login" onSubmit={handleRegister}>
+                    <h1 className="h3 mb-3 font-weight-normal">Please Register for IBS</h1>
+                    <fieldset>
+                        <label htmlFor="firstName"> First Name </label>
+                        <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="lastName"> Last Name </label>
+                        <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="username"> Username </label>
+                        <input ref={username} type="text" name="username" className="form-control" placeholder="Username" required />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input ref={email} type="email" name="email" className="form-control" placeholder="Email address" required />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit"> Sign in </button>
+                    </fieldset>
+                </form>
+            </main>
+
+            <video className="videoTag" autoPlay loop muted>
+                <source src={video} type="video/mp4" />
+            </video>
+        </>
     )
 }
