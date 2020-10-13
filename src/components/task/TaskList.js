9e47eed4f839ce5870548e1f4b209d405781f08e
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-13 18:48:50 -0500

    initial commit

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -8,33 +8,35 @@ export const TaskList = () => {
     const { tasks, getTasksByUserId } = useContext(TaskContext)
 
     const history = useHistory()
-    
+
     useEffect(() => {
-            getTasksByUserId(localStorage.getItem("nutty_user"))
+        getTasksByUserId(localStorage.getItem("nutty_user"))
     }, [])
 
     //Prevents completed task from showing up on the DOM
     const incompleteTasks = tasks.filter(task => task.completed === false)
 
-    return (	
+    return (
         <>
-            <h2>Tasks</h2>
-            <Button icon positive animated type="button" onClick={() => {
-                //takes user to task form
-                history.push("/tasks/create")
-            }}> 
-                <Button.Content visible>New Task</Button.Content>
-                <Button.Content hidden>
-                    <Icon name='plus circle' />
-                </Button.Content>
-            </Button>
-            <div className="tasks">
-                {
-                    incompleteTasks.map(task => {
-                        //creates card for each task
-                        return <TaskCard key={task.id} task={task} />
-                    })
-                }
+            <div className="taskContainer">
+                <h2>Tasks</h2>
+                <Button icon primary animated type="button" onClick={() => {
+                    //takes user to task form
+                    history.push("/tasks/create")
+                }}>
+                    <Button.Content visible>New Task</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name='plus circle' />
+                    </Button.Content>
+                </Button>
+                <div className="tasks">
+                    {
+                        incompleteTasks.map(task => {
+                            //creates card for each task
+                            return <TaskCard key={task.id} task={task} />
+                        })
+                    }
+                </div>
             </div>
         </>
     )
