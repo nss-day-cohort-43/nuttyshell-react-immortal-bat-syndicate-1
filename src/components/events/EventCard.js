a6e0abd3e699bab8f0573ff4c345bd4e81f18c02
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-13 19:24:26 -0500

    styled event list

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Event.css";
 
-export const EventCard = ({ event}) => {
+export const EventCard = ({ event }) => {
   return (
     <section className="event">
       <h3 className="event__name">
@@ -12,14 +12,14 @@ export const EventCard = ({ event}) => {
     </section>
   );
 };
-export const UpcomingEventCard = ({ event}) => {
-    return (
-      <section className="eventfirst">
-        <h2 className="event__name">
-          <Link to={`/events/detail/${event.id}`}> {event.name}</Link>
-        </h2>
-        <div>Date: {new Date(`${event.date}T07:00:00Z`).toDateString("en-US")}</div>
-      </section>
-    );
-  };
-  
+
+export const UpcomingEventCard = ({ event }) => {
+  return (
+    <section className="eventfirst">
+      <h2 className="event__name">
+        <Link to={`/events/detail/${event.id}`}> {event.name}</Link>
+      </h2>
+      <div>Date: {new Date(`${event.date}T07:00:00Z`).toDateString("en-US")}</div>
+    </section>
+  );
+};
--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -3,47 +3,54 @@ import { useHistory } from "react-router-dom";
 import { EventCard, UpcomingEventCard } from "./EventCard";
 import { EventContext } from "./EventProvider";
 import { useInterval } from "../useInterval"
-import { Checkbox } from "semantic-ui-react"
+import { Checkbox, Divider } from "semantic-ui-react"
 import "./Event.css";
 
 export const EventList = (_) => {
   const { events, getEvents } = useContext(EventContext);
-  const [ update, setUpdate ] = useState(false)
+  const [update, setUpdate] = useState(false)
   //   const [first, setFirst] = useState("true");
 
   useInterval(getEvents, update ? 3000 : null)
-  
+
   useEffect(() => {
     getEvents()
   }, []);
 
-  const filteredEvents = events.filter(event => { 
+  const filteredEvents = events.filter(event => {
     return event.date >= new Date().toISOString().split("T")[0]
-    })
+  })
 
   const history = useHistory();
   return (
     <>
-      <h2>Events</h2>
-      <Checkbox toggle 
-          onChange={() => setUpdate(!update)}
-          label={ update ? "Disable real-time updates" : "Allow real-time updates"}
-      /><br />
-      <button
-        onClick={() => {
-          history.push("events/create");
-        }}
-      >
-        Add Event
+      <div className="eventsContainer">
+        <div className="eventsHeader">
+          <h2>Events</h2>
+          <Checkbox toggle
+            onChange={() => setUpdate(!update)}
+            label={update ? "Disable real-time updates" : "Allow real-time updates"}
+          />
+        </div>
+
+        <Divider />
+
+        <button
+          onClick={() => {
+            history.push("events/create");
+          }}
+        >
+          Add Event
       </button>
-      <div className="events">
-        {filteredEvents.map((event) => {
+        <div className="events">
+          {filteredEvents.map((event) => {
             if (event.id === filteredEvents[0].id) {
               return <UpcomingEventCard key={event.id} event={event} />;
             } else {
               return <EventCard key={event.id} event={event} />;
             }
-        })}
+          })}
+        </div>
       </div>
     </>
   );
